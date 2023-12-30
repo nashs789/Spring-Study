a8107214fd3b5e4f3a120837ae630264ac0b6589
LeeInBok <bbock1214@gmail.com>
2023-12-30 19:11:20 +0900

    feat: Component Sacn

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,13 +3,17 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository repository;
 
+    @Autowired
     public MemberService(MemberRepository repository) {
         this.repository = repository;
     }
