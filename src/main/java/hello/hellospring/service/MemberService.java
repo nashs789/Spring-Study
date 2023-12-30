ab4a69f3908ffe08dfcae4c88a153004eff63b5d
LeeInBok <bbock1214@gmail.com>
2023-12-30 19:28:32 +0900

    feat: add bean in spring container

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
     private final MemberRepository repository;
 
