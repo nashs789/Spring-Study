021121bdd374108280a0946172d664f4ccf00352
LeeInBok <bbock1214@gmail.com>
2024-01-01 17:29:06 +0900

    feat: refactoring of AppConfig

--- src/main/java/hellospringbasic/core/AppConfig.java
+++ src/main/java/hellospringbasic/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hellospringbasic.core;
 
+import hellospringbasic.core.discount.DiscountPolicy;
 import hellospringbasic.core.discount.FixDiscountPolicy;
 import hellospringbasic.core.member.MemberService;
 import hellospringbasic.core.member.MemberServiceImpl;
@@ -10,10 +11,18 @@ import hellospringbasic.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
+    }
+
+    private MemoryMemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
