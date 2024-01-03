6357a0ac1dab20fdbbbf7a2db14519d0f04056d6
LeeInBok <bbock1214@gmail.com>
2024-01-04 08:56:57 +0900

    feat: duplicated bean test

--- src/main/java/hellospringbasic/core/order/OrderServiceImpl.java
+++ src/main/java/hellospringbasic/core/order/OrderServiceImpl.java
@@ -11,16 +11,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-//    @Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memId, String itemName, int itemPrice) {
