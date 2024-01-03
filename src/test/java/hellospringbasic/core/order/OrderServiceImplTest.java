ed6001efd1dab585537a6431c060f75a894cb3b8
LeeInBok <bbock1214@gmail.com>
2024-01-04 07:17:39 +0900

    feat: constructor injection

---
+++ src/test/java/hellospringbasic/core/order/OrderServiceImplTest.java
@@ -0,0 +1,23 @@
+package hellospringbasic.core.order;
+
+import hellospringbasic.core.discount.FixDiscountPolicy;
+import hellospringbasic.core.member.Grade;
+import hellospringbasic.core.member.Member;
+import hellospringbasic.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
