a1d69fe476cba663d65b187cfcf9c65de428a779
LeeInBok <bbock1214@gmail.com>
2024-01-03 14:28:30 +0900

    feat: annotation filter test code

--- src/main/java/hellospringbasic/core/AutoAppConfig.java
+++ src/main/java/hellospringbasic/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        // basePackages = "hellospringbasic.core",
+        // basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class )
 )
 public class AutoAppConfig {
---
+++ src/test/java/hellospringbasic/core/scan/filter/MyIncludeComponent.java
@@ -0,0 +1,12 @@
+package hellospringbasic.core.scan.filter;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyIncludeComponent {
+
+}
