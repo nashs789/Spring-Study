6eab5dfefcc1b0282d1b4fcbed2c462e84b67bad
LeeInBok <bbock1214@gmail.com>
2024-01-06 01:37:57 +0900

    feat: @bean(option) code test

--- src/test/java/hellospringbasic/core/lifecycle/NetworkClient.java
+++ src/test/java/hellospringbasic/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package hellospringbasic.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient{
 
     private String url;
 
@@ -29,15 +29,13 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메세지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close() {
         System.out.println("NetworkClient.destroy");
         disconnect();
     }
