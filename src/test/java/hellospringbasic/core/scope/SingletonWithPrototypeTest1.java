8cc6c858594d5632a32a15b37d5553aa5e515483
LeeInBok <bbock1214@gmail.com>
2024-01-13 14:34:01 +0900

    feat: Provider in package(jakarta.inject)

--- src/test/java/hellospringbasic/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hellospringbasic/core/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,7 @@ package hellospringbasic.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.ObjectProvider;
@@ -43,10 +44,10 @@ public class SingletonWithPrototypeTest1 {
     @Scope("singleton")
     static class ClientBean {
         @Autowired
-        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
 
             return prototypeBean.getCount();
