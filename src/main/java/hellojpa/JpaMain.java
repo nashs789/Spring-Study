7315d47ac047705130852246f66a76095a3e5ac0
nashs789 <nashs789@naver.com>
2024-01-21 20:45:28 +0900

    feat: transactional write-behind test

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -69,6 +69,14 @@ public class JpaMain {
             Member resMem2 = em.find(Member.class, 101L);
             */
 
+            // transactional write-behind
+            Member mem1 = new Member(150L, "A");
+            Member mem2 = new Member(160L, "B");
+
+            em.persist(mem1);
+            em.persist(mem2);
+            System.out.println("=================");
+
             tx.commit();
         } catch(Exception e) {
             tx.rollback();
