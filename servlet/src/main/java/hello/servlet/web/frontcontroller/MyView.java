7584ea42ba35d8a9fd85e1218c5c01b43f8efced
nashs789 <nashs789@naver.com>
2024-02-18 19:39:59 +0900

    feat: v2 view path 를 각 핸들러에서 forward 하던걸 MyView 객체 하나에서 하도록 변경

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.frontcontroller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MyView {
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    public void render(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+
+        dispatcher.forward(req, resp);
+    }
+}
