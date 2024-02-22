eb05c1737f5f770c8c8ca588bbcf1c09624ae0cc
nashs789 <nashs789@naver.com>
2024-02-22 10:52:50 +0900

    feat: ManyToMany 십습 후 ManyToOne으로 치환

--- spring-jpa-basic/src/main/java/hellojpa/Member.java
+++ spring-jpa-basic/src/main/java/hellojpa/Member.java
@@ -2,6 +2,9 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Member {
 
@@ -15,6 +18,9 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "locker_id")
     private Locker locker;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
 //    @ManyToOne(fetch = FetchType.LAZY)
 //    @JoinColumn(name = "TEAM_ID")
