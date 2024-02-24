2226e187d5ece1a7d7c0dc3c3c676096b5a69af5
nashs789 <nashs789@naver.com>
2024-02-24 15:42:48 +0900

    feat: 상속관계 매핑 실습

--- spring-jpa-basic/src/main/java/hellojpa/Member.java
+++ spring-jpa-basic/src/main/java/hellojpa/Member.java
@@ -1,48 +1,48 @@
-package hellojpa;
-
-import jakarta.persistence.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Entity
-public class Member {
-
-    @Id @GeneratedValue
-    @Column(name = "MEMBER_ID")
-    private Long id;
-
-    @Column(name = "USERNAME")
-    private String username;
-
-    @OneToOne
-    @JoinColumn(name = "locker_id")
-    private Locker locker;
-
-    @OneToMany(mappedBy = "member")
-    private List<MemberProduct> memberProducts = new ArrayList<>();
-
-    public Locker getLocker() {
-        return locker;
-    }
-
-    public void setLocker(Locker locker) {
-        this.locker = locker;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-}
+//package hellojpa;
+//
+//import jakarta.persistence.*;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Entity
+//public class Member {
+//
+//    @Id @GeneratedValue
+//    @Column(name = "MEMBER_ID")
+//    private Long id;
+//
+//    @Column(name = "USERNAME")
+//    private String username;
+//
+//    @OneToOne
+//    @JoinColumn(name = "locker_id")
+//    private Locker locker;
+//
+//    @OneToMany(mappedBy = "member")
+//    private List<MemberProduct> memberProducts = new ArrayList<>();
+//
+//    public Locker getLocker() {
+//        return locker;
+//    }
+//
+//    public void setLocker(Locker locker) {
+//        this.locker = locker;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//}
--- spring-jpa-basic/src/main/java/hellojpa/jpabook/jpashop/JpaMain.java
+++ spring-jpa-basic/src/main/java/hellojpa/jpabook/jpashop/JpaMain.java
@@ -1,28 +1,34 @@
-//package hellojpa.jpabook.jpashop;
-//
-//import hellojpa.jpabook.jpashop.domain.Order;
-//import jakarta.persistence.EntityManager;
-//import jakarta.persistence.EntityManagerFactory;
-//import jakarta.persistence.EntityTransaction;
-//import jakarta.persistence.Persistence;
-//
-//public class JpaMain {
-//    public static void main(String[] args) {
-//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-//        EntityManager em = emf.createEntityManager();
-//
-//        EntityTransaction tx = em.getTransaction();
-//        tx.begin();
-//
-//        try {
-//            Order order = new Order();
-//
-//            tx.commit();
-//        } catch(Exception e) {
-//            tx.rollback();
-//        } finally {
-//            em.close();
-//            emf.close();
-//        }
-//    }
-//}
+package hellojpa.jpabook.jpashop;
+
+import hellojpa.jpabook.jpashop.domain.Book;
+import hellojpa.jpabook.jpashop.domain.Item;
+import hellojpa.jpabook.jpashop.domain.Order;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("abc");
+
+            em.persist(book);
+
+            tx.commit();
+        } catch(Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+}
