db15c4a30d15dfee69d71b004c1a6ff40e9f97eb
nashs789 <nashs789@naver.com>
2024-02-24 18:23:03 +0900

    feat: init

---
+++ spring-socket/src/main/java/com/GreetingController.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.HtmlUtils;
+
+@RestController
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+}
