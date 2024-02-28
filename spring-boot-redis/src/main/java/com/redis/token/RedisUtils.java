be3d846cc6e9a08a1d76e87f3a47280a0cedf34d
nashs789 <nashs789@naver.com>
2024-02-28 18:00:07 +0900

    feat: Object 타입 레디스 serialize, deserialize 테스트

--- spring-boot-redis/src/main/java/com/redis/token/RedisUtils.java
+++ spring-boot-redis/src/main/java/com/redis/token/RedisUtils.java
@@ -1,10 +1,19 @@
 package com.redis.token;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.util.Optional;
+
+@Slf4j
 @Service
 public class RedisUtils {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     private final RedisTemplate<String, Object> redisTemplate;
 
@@ -12,13 +21,44 @@ public class RedisUtils {
         this.redisTemplate = redisTemplate;
     }
 
-    // concurrent.TimeUnit이 인식이 안되네
-    public void setData(String key, String value){
+    public void setStringData(String key, String value){
         redisTemplate.opsForValue().set(key, value);
     }
 
-    public String getData(String key){
+    public void setStringData(String key, String value, Duration duration){
+        redisTemplate.opsForValue().set(key, value, duration);
+    }
+
+    public String getStringData(String key){
         return (String) redisTemplate.opsForValue().get(key);
+    }
+
+    public <T> boolean setObjectData(String key, T data) {
+        try {
+            String value = objectMapper.writeValueAsString(data);
+
+            setStringData(key, value);
+
+            return true;
+        } catch(Exception ex) {
+            // TODO - 어떻게 처리할 것인지 throw 후 호출부에서 catch 잡아서 처리?
+            log.error("Redis Exception = {}", ex);
+            return false;
+        }
+    }
+
+    public <T> Optional<T> getObjectData(String key, TypeReference<T> clazz) {
+        try {
+            String value = (String) redisTemplate.opsForValue().get(key);
+
+            if(value == null) {
+                return Optional.empty();
+            }
+            return Optional.of(objectMapper.readValue(value, clazz));
+        } catch(Exception ex) {
+            log.error("Redis Exception = {}", ex);
+            return Optional.empty();
+        }
     }
 
     public void deleteData(String key){
