98a31daaaa3946023489763e927b8212114a7e58
nashs789 <nashs789@naver.com>
2024-04-08 23:22:44 +0900

    feat: summarized how to user Case

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,6 +2,7 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
@@ -387,4 +388,36 @@ public class QuerydslBasicTest {
                                       .from(member)
                                       .fetch();
     }
+
+    @Test
+    public void basicCase(){
+        List<String> fetch = queryFactory.select(member.age.when(10).then("열살")
+                                                           .when(20).then("스무살")
+                                                           .otherwise("기타")
+                                         )
+                                         .from(member)
+                                         .fetch();
+
+        for(String s : fetch) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void complexCase() {
+        List<String> fetch = queryFactory.select(new CaseBuilder()
+                                                 .when(member.age.between(0, 20)).then("0 ~ 20살")
+                                                 .when(member.age.between(21, 30)).then("21 ~ 30살")
+                                                 .otherwise("기타")
+                                         )
+                                         .from(member)
+                                         .fetch();
+
+        for(String s : fetch) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    //@Test
+
 }
